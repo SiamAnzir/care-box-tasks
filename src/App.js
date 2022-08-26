f1f61f6bf3364535bab8426b5dea0e8cb8bbb2b1
SiamAnzir <siamanzir18@gmail.com>
2022-08-27 00:14:22 +0600

    [Added] browser-routers-and-routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 import NavBar from "./components/NavBar";
 import VideoPlayer from "./components/VideoPlayer";
+import Map from "./components/Map";
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
 
 const App = () => {
   return(
-      <section>
-          <NavBar/>
-          <VideoPlayer/>
-      </section>
+      <>
+          <BrowserRouter>
+              <NavBar/>
+              <Routes>
+                  <Route exact path="/" element={<VideoPlayer/>}/>
+                  <Route exact path="/google-map" element={<Map/>}/>
+                  <Route path="*" element={ <p>"404 NOT FOUND"</p>} />
+              </Routes>
+          </BrowserRouter>
+      </>
   )
 }
 
