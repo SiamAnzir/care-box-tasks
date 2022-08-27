11098c833e1b45f8f834af7d7a6aba65a92e2b46
SiamAnzir <siamanzir18@gmail.com>
2022-08-27 14:18:58 +0600

    [Added] hash-router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import './App.css';
 import NavBar from "./components/NavBar";
 import VideoPlayer from "./components/VideoPlayer";
 import MapComponent from "./components/MapComponent";
 import {
-    BrowserRouter,
+    HashRouter,
     Routes,
     Route,
 } from "react-router-dom";
+import './App.css';
 
 const App = () => {
   return(
       <>
-          <BrowserRouter>
+          <HashRouter basename="/">
               <NavBar/>
               <Routes>
                   <Route exact path="/" element={<VideoPlayer/>}/>
                   <Route exact path="/calculate-map-distance" element={<MapComponent/>}/>
                   <Route path="*" element={ <p>"404 NOT FOUND"</p>} />
               </Routes>
-          </BrowserRouter>
+          </HashRouter>
       </>
   )
 }
